b4232707b60197969884d94a108d0b34ec96f9ba
naim edoo <naim.e2703@gmail.com>
2022-05-10 12:15:50 +0200

    add a models

--- index.js
+++ index.js
@@ -7,6 +7,43 @@ app.listen(PORT, () => {
     console.log('App is listening on port :', PORT)
 })
 
+const userSchema = new mongoose.Schema({
+    tweet:String,
+    phoneNumber:Number,
+    email:String,
+    followers:Number,
+    retweet:String,
+    comments:[{
+        body:String,
+        date:Date
+    }],
+    date : {
+        type:Date,
+        default:Date.now()
+    }
+})
+const UserModel = mongoose.model('User', userSchema)
+
+const tweetSchema = new mongoose.Schema({
+    user:{
+        type:String,
+        type:mongoose.Types.ObjectId
+    },
+        comment:String,
+        retweets:String
+})
+const TweetModel = mongoose.model('tweet',tweetSchema)
+
+const commentSchema = new mongoose.Schema({
+    user:{
+        type:String,
+        type:mongoose.Types.ObjectId
+    },
+    tweet:String    
+})
+
+const CommentModel = mongoose.model('comment', commentSchema)
+
 
 app.post('/Login',(req,res,next) => {
     res.send('OK')
